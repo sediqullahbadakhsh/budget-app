06a712e7371bd0173bee5fc3c23d7173cb3bce50
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-01 02:59:41 +0430

    validates entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,7 @@
 class Entity < ApplicationRecord
+    belongs_to :user
+    has_many :groups
+
+    validates :name, presence: {message: "Name can't be blank!"}
+    validates :amount, length: {greater_than: 0, message: 'Amount must be greater than Zero (0)'}
 end
