b1274d585793b648042b82bab08af09e2bd15837
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-01 03:08:46 +0430

    add index and reference tables

--- db/migrate/20220731212741_create_entities.rb
+++ db/migrate/20220731212741_create_entities.rb
@@ -1,8 +1,10 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.string :name
-      t.integer :amount
+      t.string :name, null: false
+      t.integer :amount, null: false
+      t.belongs_to :user, index: true, foriegn_key: true
+      t.belongs_to :group, index: true, foriegn_key: true
 
       t.timestamps
     end
--- db/migrate/20220731212910_create_groups.rb
+++ db/migrate/20220731212910_create_groups.rb
@@ -1,8 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.string :name
-      t.string :icon
+      t.string :name, null: false
+      t.string :icon, 
+      t.belongs_to :user, index: true, foriegn_key: true
+      t.belongs_to :entity, index: true, foriegn_key: true
 
       t.timestamps
     end
