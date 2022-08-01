e50311349117865e48f15387f7c07f6ff3897da6
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-01 15:36:24 +0430

    update assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many: entities
-    has_many: groups
+    has_many :entities
+    has_many :groups
 
     validates :first_name, presenece: true, length: {minimum:3, maximum: 250}
     validates :last_name, presenece: true, length: {minimum:3, maximum: 250}
--- db/migrate/20220731212910_create_groups.rb
+++ db/migrate/20220731212910_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-      t.string :icon, 
+      t.string :icon 
       t.belongs_to :user, index: true, foriegn_key: true
       t.belongs_to :entity, index: true, foriegn_key: true
 
