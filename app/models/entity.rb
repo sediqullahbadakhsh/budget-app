9d72e6f43753fc895a5277460a0c8454ddfd5d06
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-01 16:17:05 +0430

    update models and validation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,6 @@
 class Entity < ApplicationRecord
     belongs_to :user
-    has_many :groups
-
+    belongs_to :group
     validates :name, presence: {message: "Name can't be blank!"}
     validates :amount, length: {greater_than: 0, message: 'Amount must be greater than Zero (0)'}
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
     belongs_to :user
-    has_many :groups
-
+    has_many :entities
     validates :name, presence: {message: "Name can't be blank!"}
 end
