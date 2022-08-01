37869ef76020dea95a6bf8aa7c735dfb479023ad
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-01 23:30:07 +0430

    add groups and entities controllers with empty views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  get 'entities/index'
+  get 'entities/show'
+  get 'groups/index'
+  get 'groups/new'
+  root 'groups#index'
   devise_for :users
 
+  resources :users, only: :index do
+    resources :groups, only: [:index, :new, :create] do
+      resources :entities, only: [:index, :new, :create]
+    end
+  end
 end
