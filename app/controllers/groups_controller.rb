6a676541bbc8cad7a3916bac793f8e125c7591c1
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-02 00:05:40 +0430

    create and index action of group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,28 @@
 class GroupsController < ApplicationController
   def index
+    @user = current_user
+    @groups = Group.all
+    
   end
 
   def new
+    @groups = Group.new
+    @user = current_user
+
+  end
+
+  def create
+    @groups = Group.new(group_params)
+    @groups.user = current_user
+    if @groups.save
+      redirect_to user_groups_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
