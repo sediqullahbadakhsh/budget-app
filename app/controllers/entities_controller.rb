7a975812fc07e635eb78092e24d212ecb1fb53da
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-03 03:01:13 +0430

    create entity actions

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,30 @@
 class EntitiesController < ApplicationController
   def index
+    @user = current_user
+    @groups = Group.find(params[:group_id])
+    @entity = @groups.entities
   end
 
-  def show
+  def new
+    @user = current_user
+    @entity = Entity.new
+  end
+
+  def create 
+    @entity = Entity.new(entity_params)
+    @entity.user = current_user
+    @entity.group = Group.find(params[:group_id])
+    if @entity.save
+      flash[:notice] = "Entity Successfully Created"
+      redirect_to user_group_entities_path
+    else
+      flash[:alert] = "something was wrong please try again"
+      render 'new'
+    end
+  end
+  
+  private
+  def entity_params 
+    params.require(:entity).permit(:name, :amount)
   end
 end
