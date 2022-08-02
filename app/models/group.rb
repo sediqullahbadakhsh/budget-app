addf1ecbd510362250dbfbd11c8a4b1bfab9b65e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-02 23:13:29 +0430

    category icon functionality

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
     belongs_to :user
     has_many :entities
     validates :name, presence: {message: "Name can't be blank!"}
+    has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_155708) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_163052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_155708) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
