6523ca789f3d24a158ac71d2e456a668617e9472
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-04 14:30:25 +0430

    add test to group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(firstname: 'Sediq', lastname: 'badakhsh', email: 'sediq@gmail.com', password: '123456')
+    @group = Group.new(user: @user, name: 'Debit')
+  end
+
+  it 'Create a valid category' do
+    # group = Group.new(user: @user, name: 'Groceries')
+    expect(@group).to be_valid
+  end
+
+  it 'Cannot create category' do
+    group = Group.new(user: @user, name: '')
+    expect(group).to be_invalid
+    expect(group.errors[:name][0]).to be == "Name can't be blank!"
+  end
 end
